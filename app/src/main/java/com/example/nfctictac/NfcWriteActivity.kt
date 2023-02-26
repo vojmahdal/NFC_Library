23d0de9bf6df1cb445bc22c206ed39a348a8cd94
vojtamahdal <vojta@peko-studio.cz>
2023-02-26 10:33:07 +0100

    Pridani radioButton pro vyber URL a Text

--- app/src/main/java/com/example/nfctictac/NfcWriteActivity.kt
+++ app/src/main/java/com/example/nfctictac/NfcWriteActivity.kt
@@ -5,7 +5,8 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.NfcAdapter
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.easynfc.EzNfc
 
@@ -15,18 +16,39 @@ class NfcWriteActivity : AppCompatActivity() {
 
 
     private lateinit var textView : TextView
+    private lateinit var nfcText: EditText
 
     private val nfcAdapter: NfcAdapter? by lazy {
         NfcAdapter.getDefaultAdapter(this)
     }
     private var nfcLib = EzNfc( this, intentFilterArray = intentFilterArray)
 
+    private var operation = "text"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
      //   supportActionBar?.hide()
         setContentView(R.layout.nfc_write_activity)
         textView = findViewById(R.id.textNfc)
+        nfcText = findViewById(R.id.editText)
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        val radioText = findViewById<RadioButton>(R.id.radio_text)
+        val radioUrl = findViewById<RadioButton>(R.id.radio_url)
+
+
+
+        radioGroup.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener{
+                group, id ->
+                if(radioText.isChecked){
+                    operation = "text"
+                }else if(radioUrl.isChecked){
+                    operation = "url"
+                } else{
+                    operation = "text"
+                }
+            })
 
         nfcLib.nfcAdapter = nfcAdapter
 
@@ -42,12 +64,22 @@ class NfcWriteActivity : AppCompatActivity() {
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
-        nfcLib.writeText(intent, "knihovna")
-
+        if(nfcText.text.isNullOrEmpty()){
+            Toast.makeText(this, "message cannot be empty", Toast.LENGTH_SHORT).show()
+        }else {
+            if (operation == "text") {
+                nfcLib.writeText(intent, nfcText.text.toString())
+            } else if (operation == "url") {
+                nfcLib.writeUrl(intent, nfcText.text.toString())
+            } else {
+                Toast.makeText(this, "select operation", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onPause() {
      nfcLib.onPause()
         super.onPause()
     }
+
 }
